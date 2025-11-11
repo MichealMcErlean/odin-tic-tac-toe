ebb593544434944c0e950b93e4a2d38ba809483e
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-11-11 15:26:49 +1100

    Added basic Tic-Tac-Toe game flow
    Has a representation of the game-state, and can
    register moves. No victory logic yet.

---
+++ notes.md
@@ -0,0 +1,16 @@
+# General Structure
+
+Gameboard
+  getBoard
+  makeMove
+  printBoard
+
+Cell
+  setValue
+  getValue
+  isEmpty
+
+PlayerO, PlayerX
+  getToken
+
+GameController
--- script.js
+++ script.js
@@ -10,22 +10,94 @@ function Gameboard() {
       board[i].push(Cell());
     }
   }
+
+  const getBoard = () => board;
+
+  const makeMove = function(row, col, player) {
+    if (board[row][col].isEmpty()) {
+      board[row][col].setValue(player.getToken());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const reportValue = function(row, col) {
+    console.log(board[row][col].getValue());
+  }
+
+  const printBoard = function() {
+    let printedBoard = board.map((row) => row.map((cell) => cell.getValue()));
+    console.log(printedBoard);
+    console.log(reportValue(1, 1));
+  }
+
+  
+
+  return {getBoard, makeMove, printBoard, reportValue};
 }
 
 function Cell() {
-  let value = 0
+  let value = "-";
 
+  const setValue = function(token) {
+    if (value == "-") {
+      value = token;
+    } 
+  }
 
+  const getValue = function() {
+    return value;
+  }
+
+  const isEmpty = function() {
+    return (value == "-");
+  }
+
+  return {getValue, setValue, isEmpty};
 }
 
 function PlayerO() {
+  let token = "O";
 
+  const getToken = function() {
+    return token;
+  };
+
+  return {getToken}
 }
 
 function PlayerX() {
+  let token = "X";
 
+  const getToken = function() {
+    return token;
+  };
+
+  return {getToken};
 }
 
 function GameController() {
+  const board = Gameboard();
+  const playerO = PlayerO();
+  const playerX = PlayerX();
+  
+  let activePlayer = Math.random() > 0.5 ? playerO : playerX;
 
+  const getActivePlayer = () => activePlayer;
+
+  const swapActivePlayer = () => activePlayer = activePlayer === playerO ? playerX : playerO;
+
+  const playTurn = (row, col) => {
+    board.makeMove(row, col, activePlayer);
+
+    swapActivePlayer();
+    printNewRound();
+  };
+
+  const printNewRound = function() {
+    board.printBoard();
+  }
+
+  return {printNewRound, playTurn};
 }
